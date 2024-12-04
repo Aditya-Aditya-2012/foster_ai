f8c6ad7c2249a15f824ade9c50116c9b729b85b7
Aditya <aditya20rajan@gmail.com>
2024-12-04 21:10:49 +0530

    Add styling to match wireframe designs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import React, {useState, useEffect} from "react";
 import TemplateList from "./components/TemplateList";
 import TemplateEditor from "./components/TemplateEditor";
