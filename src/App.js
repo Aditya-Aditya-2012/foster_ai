b0c7f9a7ee14fabf9f252a893d0a1bf93941aebd
Aditya <aditya20rajan@gmail.com>
2024-12-04 20:42:55 +0530

    Implement template creation form with validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import TemplateList from "./components/TemplateList";
 import TemplateEditor from "./components/TemplateEditor";
 
 function App() {
+  const [templates, setTemplates] = useState([])
+  const [selectedTemplate, setSelectedTemplate] = useState(null);
+
+  const handleSave = (newTemplate) => {
+    setTemplates((prev) => {
+      const updatedTemplates = [...prev, newTemplate];
+      localStorage.setItem("templates", JSON.stringify(updatedTemplates));
+      return updatedTemplates;
+    });
+    setSelectedTemplate(null);
+  };
   return (
     <div>
       <h1>Template Manager</h1>
-      <TemplateList />
-      <TemplateEditor />
+      <TemplateList
+        templates = {templates}
+        onSelect = {(template) => setSelectedTemplate(template)}
+      />
+      <TemplateEditor template = {selectedTemplate} onSave={handleSave} />
     </div>
   );
 }
